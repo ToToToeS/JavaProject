d36e8ac6935fd043d63b3d702cf567d5a480084e
Shatilo AI <rachinacom@mail.ru>
2024-12-29 17:22:08 +0300

    I solve 8 problem and now i know what is graph and how work with it

--- Grokaem Algoritms/src/Graph.java
+++ Grokaem Algoritms/src/Graph.java
@@ -1,11 +1,50 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashMap;
+
 
 public class Graph {
     public static void main(String args[]){
+        System.out.println(search(getGraph()));
+        String s = "fdf";
+    }
+
+    public static HashMap<String, String[]> getGraph(){
         HashMap<String, String[]> graph = new HashMap<>();
 
         graph.put("you", new String[]{"alice", "bob", "claire"});
+        graph.put("bob", new String[]{"peggi", "anuj"});
+        graph.put("alice", new String[]{"peggy"});
+        graph.put("claire", new String[]{"tom", "jonny"});
+        graph.put("tom", null);
+        graph.put("jonny", null);
+        graph.put("anuj", null);
+        graph.put("peggy", null);
 
-        System.out.println(graph);
+        return graph;
+    }
+
+    public static String search(HashMap<String, String[]> graph){
+        Deque<String> que = new ArrayDeque<>();
+        que.add("you");
+        while(!que.isEmpty()){
+            String person = que.pollFirst();
+            if(person.equals("claire")){
+                return person;
+            }else{
+                String[] friends = graph.get(person);
+                for(int i = 0; i < friends.length; i++){
+                    if(friends[i] != null && !que.contains(friends[i])){
+                        que.add(friends[i]);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public static boolean isMangoSeller(String person){
+
+        return false;
     }
 }
