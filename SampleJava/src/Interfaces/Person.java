4910364c4479991f7e5db75195b21878bf3811bf
Shatilo AI <rachinacom@mail.ru>
2024-12-31 14:55:49 +0300

    My progress on 31.12.2024

---
+++ SampleJava/src/Interfaces/Person.java
@@ -0,0 +1,17 @@
+package Interfaces;
+
+public class Person implements Info{
+    public String name;
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public void sayHello() {
+        System.out.println("HEllO");
+    }
+
+    public void showInfo(){
+        System.out.println("ID is " + this.name);
+    }
+}
---
+++ SampleJava/src/inheritance.java
@@ -0,0 +1,22 @@
+public class inheritance {
+    public static void main(String args[]){
+        Dog dog = new Dog();
+        dog.gow();
+    }
+}
+
+class Animal{
+    public void eat(){
+        System.out.println("I'me eating");
+    }
+
+    public void sleep(){
+        System.out.println("I'm sleep");
+    }
+}
+
+class Dog extends Animal {
+    public void gow(){
+        System.out.println("GOOW");
+    }
+}
